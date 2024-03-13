790975f4f83572705135557679b640575b110158
chathun <chathunworkmail@gmail.com>
2024-03-13 17:20:50 +0530

    test commit

--- lambda.py
+++ lambda.py
@@ -1,5 +1,6 @@
 def lambda_handler(event, context):
     # Respond with a 200 OK status and a message indicating the service is healthy
+    #test commit
     return {
         'statusCode': 200,
         'body': 'Service is healthy'
